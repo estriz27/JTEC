b070e641bb0d81bfce74d7302e99afeb7ceffd04
Elias <ebs021@bucknell.edu>
2017-04-05 15:08:06 -0400

    adding stuff

--- FrontEnd/client/Plotly.js
+++ FrontEnd/client/Plotly.js
@@ -1,6 +1,6 @@
-
-//		var a = 1000;
-//		var b = 1000;
+/**
+* create_blank_array() returns an array of 0's with dimensions axb
+*/
 
 		function create_blank_array(a,b)
 		{
@@ -18,9 +18,19 @@
 		return array;
 		}
 
+		/**
+		* populate_array() returns an array based on screen_size where the value at each index represents
+		*the amount of times that xy coordinate appears in the x_array, y_array.
+		*For example, x_array = [1,1,2,3] y_array = [1,1,3,4], screen_size [4,4] generates
+		*[[0,0,0,0,0],[0,2,0,0,0],[0,0,0,0,0],[0,0,1,0,0],[0,0,0,1,0]]
+		*/
 	function populate_array(x_array, y_array,screen_size){
 		var min_size = Math.min(x_array.length,y_array.length);
 		var array = create_blank_array(screen_size[1]+1,screen_size[0]+1);
+		//If statement triggers if there is a value in x_array or y_array that is out of bounds of the screen
+		if (min_size > (screen_size[1]+1) || min_size > (screen_size[0]+1)){
+			alert("screen size too small");
+		}
 		for (i = 0; i < min_size; i++){
 			var x = y_array[i];
 			var y = x_array[i];
@@ -28,11 +38,10 @@
 		}
 		return array;
 	}
-
+/**
+*make_heatmap() generates a plotly heatmap based on the input values
+*/
 function make_heatmap(x_array, y_array, screen_size){
-console.log(x_array);
-console.log(y_array);
-screen_size = [1000,1000];
 			var data = [
 		  {
 		    z: populate_array(x_array, y_array,screen_size),
